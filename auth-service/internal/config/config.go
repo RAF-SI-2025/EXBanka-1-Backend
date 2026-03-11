bab9fd84899c3b267eb65512dda2eb7cbf934402
lukasavic <lukavicsa12@gmail.com>
2026-03-12 00:16:53 +0100

    .env fix

--- auth-service/internal/config/config.go
+++ auth-service/internal/config/config.go
@@ -23,17 +23,21 @@ type Config struct {
 }
 
 func Load() *Config {
-	godotenv.Load()
+	for _, f := range []string{".env", "../.env", "../../.env", "../../../.env"} {
+		if err := godotenv.Load(f); err == nil {
+			break
+		}
+	}
 
 	accessExp, _ := time.ParseDuration(getEnv("JWT_ACCESS_EXPIRY", "15m"))
 	refreshExp, _ := time.ParseDuration(getEnv("JWT_REFRESH_EXPIRY", "168h"))
 
 	return &Config{
-		DBHost:        getEnv("AUTH_DB_HOST", "localhost"),
-		DBPort:        getEnv("AUTH_DB_PORT", "5433"),
-		DBUser:        getEnv("AUTH_DB_USER", "postgres"),
-		DBPassword:    getEnv("AUTH_DB_PASSWORD", "postgres"),
-		DBName:        getEnv("AUTH_DB_NAME", "auth_db"),
+		DBHost:        getEnv("AUTH_DB_HOST", "exteam1-database.postgres.database.azure.com"),
+		DBPort:        getEnv("AUTH_DB_PORT", "5432"),
+		DBUser:        getEnv("AUTH_DB_USER", "exteam1"),
+		DBPassword:    getEnv("AUTH_DB_PASSWORD", "Anajankovic03"),
+		DBName:        getEnv("AUTH_DB_NAME", "authservicedb"),
 		GRPCAddr:      getEnv("AUTH_GRPC_ADDR", ":50051"),
 		UserGRPCAddr:  getEnv("USER_GRPC_ADDR", "localhost:50052"),
 		KafkaBrokers:  getEnv("KAFKA_BROKERS", "localhost:9092"),
@@ -52,7 +56,7 @@ func getEnv(key, fallback string) string {
 }
 
 func (c *Config) DSN() string {
-	sslmode := getEnv("AUTH_DB_SSLMODE", "disable")
+	sslmode := getEnv("AUTH_DB_SSLMODE", "require")
 	return "host=" + c.DBHost +
 		" port=" + c.DBPort +
 		" user=" + c.DBUser +
